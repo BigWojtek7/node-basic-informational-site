93035a7106032fd4a087e4f62ce45c85c671df48
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-05-31 14:01:30 +0200

    Add basic style and 404 error page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import http from "node:http";
+import fs from "node:fs";
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url);
-    res.end();
+    const q = new URL(`http://${req.headers.host}${req.url}`);
+
+
+
+
+    const filename =
+      q.pathname === "/" ? "./index.html" : `.${q.pathname}.html`;
+
+    
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+         const errorPage = fs.readFileSync('./404.html')
+        res.write(errorPage);
+        return res.end();
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080);
