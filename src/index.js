329c64d02b71bc608755d1fdfd25959d36360ab2
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-05-31 09:25:43 +0200

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import http from "node:http";
 
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(req.url);
+    res.end();
+  })
+  .listen(8080);
