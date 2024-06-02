689328493de9086a4627b06f8452f01f099129d0
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-06-02 20:35:53 +0200

    Add 404 error handler

--- src/index.js
+++ src/index.js
@@ -17,29 +17,9 @@ app.get('/contact-me', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server listening on port ${port}`)
-})
+  console.log(`server listening on port ${port}`);
+});
 
-// import http from "node:http";
-// import fs from "node:fs";
-
-// http
-//   .createServer((req, res) => {
-//     const q = new URL(`http://${req.headers.host}${req.url}`);
-
-//     const filename =
-//       q.pathname === "/" ? "./index.html" : `.${q.pathname}.html`;
-
-//     fs.readFile(filename, (err, data) => {
-//       if (err) {
-//         res.writeHead(404, { "Content-Type": "text/html" });
-//         const errorPage = fs.readFileSync("./404.html");
-//         res.write(errorPage);
-//         return res.end();
-//       }
-//       res.writeHead(200, { "Content-Type": "text/html" });
-//       res.write(data);
-//       return res.end();
-//     });
-//   })
-//   .listen(8080);
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
