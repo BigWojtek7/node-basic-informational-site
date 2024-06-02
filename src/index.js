9a6c8ebc1e1b4bb681186c97f916b93a77d96578
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-06-02 20:27:13 +0200

    Change index.js to work with express.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-import http from "node:http";
-import fs from "node:fs";
+const express = require('express');
+const path = require('path');
 
-http
-  .createServer((req, res) => {
-    const q = new URL(`http://${req.headers.host}${req.url}`);
+const app = express();
+const port = 8080;
 
-    const filename =
-      q.pathname === "/" ? "./index.html" : `.${q.pathname}.html`;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        const errorPage = fs.readFileSync("./404.html");
-        res.write(errorPage);
-        return res.end();
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact-me.html'));
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+})
+
+// import http from "node:http";
+// import fs from "node:fs";
+
+// http
+//   .createServer((req, res) => {
+//     const q = new URL(`http://${req.headers.host}${req.url}`);
+
+//     const filename =
+//       q.pathname === "/" ? "./index.html" : `.${q.pathname}.html`;
+
+//     fs.readFile(filename, (err, data) => {
+//       if (err) {
+//         res.writeHead(404, { "Content-Type": "text/html" });
+//         const errorPage = fs.readFileSync("./404.html");
+//         res.write(errorPage);
+//         return res.end();
+//       }
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//   })
+//   .listen(8080);
